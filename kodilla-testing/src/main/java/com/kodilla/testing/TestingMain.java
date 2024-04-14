171e2e58d79f10478010b6e2e9970f503dfb55bd
Jakub Zygula <zygulajakub@gmail.com>
2024-04-14 08:30:34 +0200

    Moduł 6.2: Zadanie: Pierwszy test jednostkowy

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,2 +1,24 @@
-package com.kodilla.testing;public class TestingMain {
+package com.kodilla.testing;
+import com.kodilla.testing.calculator.Calculator;
+
+public class TestingMain {
+    public static void main(String[] args) {
+        System.out.println("Moduł 6: Wprowadzenie to testowania oprogramowania");
+
+        System.out.println("Moduł 6.2: Zadanie: Pierwszy test jednostkowy - POCZĄTEK");
+        Calculator calculator = new Calculator();
+        int addResult = 24;
+        int subtractResult = 9;
+        if(addResult == calculator.add(15, 9)) {
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+        if(subtractResult == calculator.subtract(24, 15)) {
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+        System.out.println("Moduł 6.2: Zadanie: Pierwszy test jednostkowy - KONIEC");
+    }
 }
---
+++ kodilla-testing/src/main/java/com/kodilla/testing/calculator/Calculator.java
@@ -0,0 +1,11 @@
+package com.kodilla.testing.calculator;
+
+//Moduł 6.2: Zadanie: Pierwszy test jednostkowy
+public class Calculator {
+    public int add(int a, int b) {
+        return a+b;
+    }
+    public int subtract(int a, int b) {
+        return a-b;
+    }
+}
