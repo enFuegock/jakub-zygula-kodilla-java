3afe91f0fb4eb2542ff5f3767cf7bfb53183f2f0
Jakub Zygula <zygulajakub@gmail.com>
2024-04-29 23:08:09 +0200

    Modul 8.2 try! catch! finally!

---
+++ kodilla-additionalTasks/src/main/java/com/kodilla/wearepower/CalculatePowerOfAnX.java
@@ -0,0 +1,18 @@
+package com.kodilla.wearepower;
+
+class PowerOfX {
+    private int a;
+    private int n;
+
+    public PowerOfX(int a, int n) {
+
+    }
+}
+
+
+public class CalculatePowerOfAnX {
+    public static void main(String[] args) {
+
+
+    }
+}
