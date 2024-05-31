f415d4b36b5ed49912bac620ace85a52d59d786f
Jakub Zygula <zygulajakub@gmail.com>
2024-05-31 10:17:03 +0200

    M 14.1

--- kodilla-additionalTasks/src/main/java/com/kodilla/fibonacci/FibonacciSequence.java
+++ kodilla-additionalTasks/src/main/java/com/kodilla/fibonacci/FibonacciSequence.java
@@ -1,20 +1,20 @@
 package com.kodilla.fibonacci;
 
-import java.util.Scanner;
-
 class FibonacciSequence {
 
     public static void main(String[] args) {
+        int x = CalNthFibSeq(15);
+        System.out.println("wynik dla 15: "+ x);
+        int a = CalNthFibSeq(21);
+        System.out.println("wynik dla 21: "+ a);
     }
 
-    public static int CalNthFibSeq(int n){
-        Scanner input = new Scanner(System.in);
-        System.out.println("Enter the value of n:");
-        n = input.nextInt();
+    public static int CalNthFibSeq(int n) {
         int[] fibSeqN = new int[n+1];
         fibSeqN[0] = 0;
         fibSeqN[1] = 1;
         for (int i = 2; i <= n; i++) {
+
             fibSeqN[i] = fibSeqN[i - 1] + fibSeqN[i - 2];
         }
         return fibSeqN[n];
